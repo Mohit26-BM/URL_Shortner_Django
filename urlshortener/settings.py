c5c08e91265a751b673f813d03b39d1c6aa02e69
Mohit Bali <m26ohitbali@gmail.com>
2026-02-25 15:19:44 +0530

    Change database configuration to use SQLite

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -94,10 +94,10 @@ WSGI_APPLICATION = "urlshortener.wsgi.application"
 
 
 DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
